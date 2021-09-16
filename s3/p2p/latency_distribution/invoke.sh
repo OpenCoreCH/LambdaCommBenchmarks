08c10b6480e7ae5d1c2863c6dc3a96206f1ef360
Roman Böhringer <r.boehringer@opencore.ch>
2021-09-16 17:07:11 +0200

    Rerun latency_distribution with correct timestamp

--- s3/p2p/latency_distribution/invoke.sh
+++ s3/p2p/latency_distribution/invoke.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir -p out/
 
-timestamp=$(date +%s)
 filesizes=(1 1000 10000 100000 1000000 10000000 100000000)
 filesize=10000000
 
 for run in {1..100}
     do
+        timestamp=$(date +%s)
         aws lambda invoke --function-name s3benchmark --cli-binary-format raw-in-base64-out --payload '{"s3bucket": "romanboe-test", "s3key":"'"$timestamp"'", "role": "consumer", "fileSize": '"$filesize"' }' "out/consumer_${filesize}_${run}.json" &
         aws lambda invoke --function-name s3benchmark --cli-binary-format raw-in-base64-out --payload '{"s3bucket": "romanboe-test", "s3key":"'"$timestamp"'", "role": "producer", "fileSize": '"$filesize"' }' "out/producer_${filesize}_${run}.json" &
         sleep 5
