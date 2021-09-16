1a9bff54cdd0f58336791c320b04c1d0f99dedec
Roman Böhringer <r.boehringer@opencore.ch>
2021-09-16 14:32:08 +0200

    S3 Latency distribution experiment

--- s3/p2p/invoke.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-timestamp=$(date +%s)
-filesizes=(1 1000 10000 100000 1000000 10000000 100000000)
-filesize=100000
-
-#aws lambda invoke --function-name s3benchmark --cli-binary-format raw-in-base64-out --payload '{"s3bucket": "romanboe-test", "s3key":"'"$timestamp"'", "role": "consumer", "fileSize": '"$filesize"' }' "out/consumer_${filesize}_1.json" &
-aws lambda invoke --function-name s3benchmark --cli-binary-format raw-in-base64-out --payload '{"s3bucket": "romanboe-test", "s3key":"'"$timestamp"'", "role": "producer", "fileSize": '"$filesize"' }' "out/producer_${filesize}_1.json" &
