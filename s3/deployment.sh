a716ce595937b3aa9529494a71ca6583ef13aabc
Roman Böhringer <r.boehringer@opencore.ch>
2021-09-18 20:00:02 +0200

    Updated deployment script

--- s3/deployment.sh
+++ s3/deployment.sh
@@ -1,17 +1,20 @@
+# Creating role
 aws iam create-role --role-name lambda-s3 --assume-role-policy-document file://trust-policy.json
 aws iam attach-role-policy --role-name lambda-s3 --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
 aws iam attach-role-policy --role-name lambda-s3 --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
 
+# Creating function
 aws lambda create-function \
 --function-name s3benchmark \
 --role arn:aws:iam::690111777418:role/lambda-cpp-demo \
 --runtime provided \
---timeout 15 \
---memory-size 128 \
+--timeout 60 \
+--memory-size 2048 \
 --handler s3benchmark \
 --zip-file fileb://s3benchmark.zip
 
+# Compiling and packaging code into zip file
 make aws-lambda-package-s3benchmark
 
+# Updating code with newest zip
 aws lambda update-function-code --function-name s3benchmark --zip-file fileb://s3benchmark.zip
-
