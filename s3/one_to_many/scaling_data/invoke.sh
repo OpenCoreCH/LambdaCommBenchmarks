b75077540f3a79068f110de366bb9287d213bbdd
Roman Böhringer <r.boehringer@opencore.ch>
2021-09-18 12:49:03 +0200

    One to many scaling data benchmark

---
+++ s3/one_to_many/scaling_data/invoke.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir -p out/
+
+filesizes=(1 1000 10000 100000 1000000 10000000 100000000 1000000000)
+consumer=8
+
+for filesize in "${filesizes[@]}"
+do
+    for run in {1..10}
+    do
+        timestamp=$(date +%s)
+        aws lambda invoke --function-name s3benchmark --cli-binary-format raw-in-base64-out --payload '{"s3bucket": "romanboe-test", "s3key":"'"$timestamp"'", "role": "producer", "fileSize": '"$filesize"' }' "out/producer_${filesize}_${run}.json" &
+        for consumernum in $(seq 1 $consumer);
+            do
+            aws lambda invoke --function-name s3benchmark --cli-binary-format raw-in-base64-out --payload '{"s3bucket": "romanboe-test", "s3key":"'"$timestamp"'", "role": "consumer", "fileSize": '"$filesize"' }' "out/consumer_${filesize}_${run}_${consumernum}.json" &
+            done
+        sleep 10
+    done
+done
