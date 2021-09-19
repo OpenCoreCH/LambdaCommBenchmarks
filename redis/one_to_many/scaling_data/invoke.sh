955efa80c94d70315a1e0bbf3ec3af9ba2c3559e
Roman Böhringer <r.boehringer@opencore.ch>
2021-09-19 23:59:40 +0200

    Ran redis 500MB benchmark with 8 clients using a large instance

--- redis/one_to_many/scaling_data/invoke.sh
+++ redis/one_to_many/scaling_data/invoke.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 mkdir -p out/
 
-REDIS_HOSTNAME=clusterforlambda.2ctj87.0001.euc1.cache.amazonaws.com
+REDIS_HOSTNAME=clusterlarge.2ctj87.0001.euc1.cache.amazonaws.com
 REDIS_PORT=6379
 filesizes=(1 1000 10000 100000 1000000 10000000 100000000 500000000)
-filesizes=(500000000) # Need larger redis instance for this filesize
 consumer=8
 
 for filesize in "${filesizes[@]}"
@@ -17,6 +16,6 @@ do
             do
             aws lambda invoke --function-name redisbenchmark --cli-binary-format raw-in-base64-out --payload '{"hostname":"'"$REDIS_HOSTNAME"'", "port":'"$REDIS_PORT"', "key":"'"$timestamp"'", "role": "consumer", "fileSize": '"$filesize"' }' "out/consumer_${filesize}_${run}_${consumernum}.json" &
             done
-        sleep 10
+        sleep 130
     done
 done
