459d9fe44e2a9e6469286565da5d3aef3e40a241
Roman Böhringer <r.boehringer@opencore.ch>
2021-09-20 22:23:15 +0200

    Working prototype for NAT file transfer, sometimes not completing

--- nat/main.cpp
+++ nat/main.cpp
@@ -16,12 +16,12 @@ using boost::asio::ip::udp;
 using namespace aws::lambda_runtime;
 char const TAG[] = "LAMBDA_ALLOC";
 
-/*uint64_t upload_random_file(redisContext* context,
-                        std::string const &key,
-                        int size);
+uint64_t send_file(udp::socket& socket,
+                   udp::endpoint peer,
+                   int size);
 
-uint64_t download_file(redisContext* context,
-                       std::string const &key);*/
+uint64_t get_file(udp::socket&socket,
+                  udp::endpoint peer);
 
 uint64_t timeSinceEpochMillisec()
 {
@@ -57,32 +57,19 @@ static invocation_response my_handler(invocation_request const &req)
     boost::asio::io_service io_service;
     udp::socket socket(io_service);
     boost::system::error_code err;
-    std::cout << "Calling pair " << std::endl;
     udp::endpoint peer = pair(socket, pairing_key, server_ip);
 
     std::string res_json = "{ \"fileSize\": " + std::to_string(file_size) + ", \"role\": \"" + role + "\"" ;
     if (role == "producer")
     {
-        uint64_t upload_time = 0;
-        socket.send_to(boost::asio::buffer("Ping"), peer, 0, err);
-        if (err)
-            throw boost::system::system_error(err);
-        char recv_buffer[MAX_BUFFER_DATA_SIZE];
-        std::cout << "Waiting for recv" << std::endl;
-	    socket.receive_from(boost::asio::buffer(recv_buffer), peer);
-	    std::cout << recv_buffer << std::endl;
-        //uint64_t upload_time = upload_random_file(c, key, file_size);
+        uint64_t upload_time = send_file(socket, peer, file_size);
         res_json += ", \"uploadTime\": " + std::to_string(upload_time) + " }";
     }
     else if (role == "consumer")
     {
-        uint64_t finished_time = 0;
-        socket.send_to(boost::asio::buffer("Ping"), peer, 0, err);
-        char recv_buffer[MAX_BUFFER_DATA_SIZE];
-        std::cout << "Waiting for recv" << std::endl;
-	    socket.receive_from(boost::asio::buffer(recv_buffer), peer);
-	    std::cout << recv_buffer << std::endl;
-        //uint64_t finished_time = download_file(c, key);
+        socket.send_to(boost::asio::buffer("1"), peer, 0, err);
+        
+        uint64_t finished_time = get_file(socket, peer);
         res_json += ", \"finishedTime\": " + std::to_string(finished_time) + " }";
     }
     
@@ -116,41 +103,48 @@ int main()
     return 0;
 }
 
-/*uint64_t download_file(redisContext* context,
-                       std::string const &key)
+uint64_t get_file(udp::socket&socket,
+                  udp::endpoint peer)
 {
-    int retries = 0;
-    const int MAX_RETRIES = 50000;
-    while (retries < MAX_RETRIES) {
-        std::string comm = "GET " + key;
-        redisReply* reply = (redisReply*) redisCommand(context, comm.c_str());
-        if (reply->type == REDIS_REPLY_NIL || reply->type == REDIS_REPLY_ERROR) {
-            retries += 1;
-            freeReplyObject(reply);
-        } else {
-            uint64_t finishedTime = timeSinceEpochMillisec();
-            // Perform NOP on result to prevent optimizations
-            for (int i = 0; i < reply->elements; i++) {
-                std::string res = reply->element[i]->str;
-            }
-            freeReplyObject(reply);
-            return finishedTime;
-        }
+    char recv_buffer[MAX_BUFFER_DATA_SIZE];
+	int n = socket.receive_from(boost::asio::buffer(recv_buffer), peer);
+    std::cout << "Got " << n << " bytes " << std::endl;
+    int size = *((int*) recv_buffer);
+    std::cout << "Total data length " << size << std::endl;
+    int received_size = n - sizeof(int);
+    char* out_buffer = new char[size];
+    memcpy(out_buffer, recv_buffer + sizeof(int), n - sizeof(int));
+    int num_packets = (size + sizeof(int) + MAX_BUFFER_DATA_SIZE - 1) / MAX_BUFFER_DATA_SIZE; // int ceil of (size + sizeof(int))/MAX_BUFFER_DATA_SIZE
+    for (int i = 1; i < num_packets; i++) {
+        int n = socket.receive_from(boost::asio::buffer(recv_buffer), peer);
+        std::cout << "Got " << n << " bytes " << std::endl;
+        memcpy(out_buffer + received_size, recv_buffer, n);
+        received_size += n;
     }
-    return 0;
-
+    
+    uint64_t after = timeSinceEpochMillisec();
+    delete[] out_buffer;
+	
+    return after;
 }
 
-uint64_t upload_random_file(redisContext* context,
-                        std::string const &key,
-                        int size)
+uint64_t send_file(udp::socket& socket,
+                   udp::endpoint peer,
+                   int size)
 {
-    char* pBuf = new char[size];
+    int tot_size = size + sizeof(int);
+    char* pBuf = new char[tot_size];
+    *((int*) pBuf) = size;
+    int num_packets = (tot_size + MAX_BUFFER_DATA_SIZE - 1) / MAX_BUFFER_DATA_SIZE; // int ceil
 
     uint64_t bef_upload = timeSinceEpochMillisec();
-    std::string comm = "SET " + key + " %b";
-    redisReply* reply = (redisReply*) redisCommand(context, comm.c_str(), pBuf, size);
-    freeReplyObject(reply);
+    int remaining_size = tot_size;
+    for (int i = 0; i < num_packets; i++) {
+        std::cout << "Sending packet " << i << std::endl;
+        int packet_size = std::min(remaining_size, MAX_BUFFER_DATA_SIZE);
+        socket.send_to(boost::asio::buffer(pBuf + i * MAX_BUFFER_DATA_SIZE, packet_size), peer, 0);
+        remaining_size -= packet_size;
+    }
     delete[] pBuf;
     return bef_upload;
-}*/
+}
--- nat/p2p/latency_distribution/invoke.sh
+++ nat/p2p/latency_distribution/invoke.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p out/
 
-SERVER_IP=18.195.20.33
+SERVER_IP=18.184.154.126
 
 filesizes=(1 1000 10000 100000 1000000 10000000 100000000)
 filesize=10000000
@@ -9,7 +9,8 @@ filesize=10000000
 for run in {1..1}
     do
         timestamp=$(date +%s)
-        aws lambda invoke --function-name natbenchmark --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"$timestamp"'", "role": "producer", "fileSize": '"$filesize"' }' "out/producer_${filesize}_${run}.json" &
         aws lambda invoke --function-name natbenchmark --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"$timestamp"'", "role": "consumer", "fileSize": '"$filesize"' }' "out/consumer_${filesize}_${run}.json" &
+        sleep 1
+        aws lambda invoke --function-name natbenchmark --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"$timestamp"'", "role": "producer", "fileSize": '"$filesize"' }' "out/producer_${filesize}_${run}.json" &
         sleep 5
     done
