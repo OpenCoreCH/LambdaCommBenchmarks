7657a5b2df67c71ea10b2188e8a0e0f0b4897840
Roman Böhringer <r.boehringer@opencore.ch>
2021-09-20 19:21:56 +0200

    Started working on NAT benchmark

---
+++ nat/deployment.sh
@@ -0,0 +1,26 @@
+# Creating role
+aws iam create-role --role-name lambda-vpc-role --assume-role-policy-document file://trust-policy.json
+aws iam attach-role-policy --role-name lambda-vpc-role --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+aws iam attach-role-policy --role-name lambda-vpc-role --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole
+
+# Creating EC2 VM (group -> default VPC security group, incoming UDP connections on port 10,000 allowed)
+aws ec2 run-instances --image-id ami-07df274a488ca9195 --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-06df67e1deee61365 --subnet-id subnet-02ace63723abfa440
+
+# To get the IP
+aws ec2 describe-instances
+
+# Creating function
+aws lambda create-function \
+--function-name natbenchmark \
+--role arn:aws:iam::690111777418:role/lambda-vpc-role \
+--runtime provided \
+--timeout 60 \
+--memory-size 2048 \
+--handler natbenchmark \
+--zip-file fileb://natbenchmark.zip
+
+# Compiling and packaging code into zip file
+make aws-lambda-package-natbenchmark
+
+# Updating code with newest zip
+aws lambda update-function-code --function-name natbenchmark --zip-file fileb://natbenchmark.zip
---
+++ nat/main.cpp
@@ -0,0 +1,156 @@
+#include <aws/core/Aws.h>
+#include <aws/core/utils/logging/LogLevel.h>
+#include <aws/core/utils/logging/ConsoleLogSystem.h>
+#include <aws/core/utils/logging/LogMacros.h>
+#include <aws/core/utils/json/JsonSerializer.h>
+#include <aws/lambda-runtime/runtime.h>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <cstdint>
+#include <iostream>
+#include <hole_punching_client.h>
+
+using boost::asio::ip::udp;
+using namespace aws::lambda_runtime;
+char const TAG[] = "LAMBDA_ALLOC";
+
+/*uint64_t upload_random_file(redisContext* context,
+                        std::string const &key,
+                        int size);
+
+uint64_t download_file(redisContext* context,
+                       std::string const &key);*/
+
+uint64_t timeSinceEpochMillisec()
+{
+  using namespace std::chrono;
+  return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+
+static invocation_response my_handler(invocation_request const &req)
+{
+    using namespace Aws::Utils::Json;
+    JsonValue json(req.payload);
+    if (!json.WasParseSuccessful())
+    {
+        return invocation_response::failure("Failed to parse input JSON", "InvalidJSON");
+    }
+
+    auto v = json.View();
+
+    auto pairing_key = v.GetString("key");
+    auto server_ip = v.GetString("ip");
+    auto role = v.GetString("role"); // producer or consumer
+    auto file_size = v.GetInteger("fileSize");
+    if (v.KeyExists("waitUntil")) {
+        auto wait_until = v.GetInteger("waitUntil");
+        while (std::time(0) < wait_until) {
+            
+        }
+
+    }
+    std::cout << "Invoked handler for role " << role << " with file size " << file_size << std::endl;
+
+    boost::asio::io_service io_service;
+    udp::socket socket(io_service);
+    boost::system::error_code err;
+    std::cout << "Calling pair " << std::endl;
+    udp::endpoint peer = pair(socket, pairing_key, server_ip);
+
+    std::string res_json = "{ \"fileSize\": " + std::to_string(file_size) + ", \"role\": \"" + role + "\"" ;
+    if (role == "producer")
+    {
+        uint64_t upload_time = 0;
+        socket.send_to(boost::asio::buffer("Ping"), peer, 0, err);
+        if (err)
+            throw boost::system::system_error(err);
+        char recv_buffer[MAX_BUFFER_DATA_SIZE];
+        std::cout << "Waiting for recv" << std::endl;
+	    socket.receive_from(boost::asio::buffer(recv_buffer), peer);
+	    std::cout << recv_buffer << std::endl;
+        //uint64_t upload_time = upload_random_file(c, key, file_size);
+        res_json += ", \"uploadTime\": " + std::to_string(upload_time) + " }";
+    }
+    else if (role == "consumer")
+    {
+        uint64_t finished_time = 0;
+        socket.send_to(boost::asio::buffer("Ping"), peer, 0, err);
+        char recv_buffer[MAX_BUFFER_DATA_SIZE];
+        std::cout << "Waiting for recv" << std::endl;
+	    socket.receive_from(boost::asio::buffer(recv_buffer), peer);
+	    std::cout << recv_buffer << std::endl;
+        //uint64_t finished_time = download_file(c, key);
+        res_json += ", \"finishedTime\": " + std::to_string(finished_time) + " }";
+    }
+    
+    return invocation_response::success(res_json, "application/json");
+}
+
+std::function<std::shared_ptr<Aws::Utils::Logging::LogSystemInterface>()> GetConsoleLoggerFactory()
+{
+    return []
+    {
+        return Aws::MakeShared<Aws::Utils::Logging::ConsoleLogSystem>(
+            "console_logger", Aws::Utils::Logging::LogLevel::Info);
+    };
+}
+
+int main()
+{
+    using namespace Aws;
+    SDKOptions options;
+    options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Info;
+    options.loggingOptions.logger_create_fn = GetConsoleLoggerFactory();
+    InitAPI(options);
+    {
+        auto handler_fn = [](aws::lambda_runtime::invocation_request const &req)
+        {
+            return my_handler(req);
+        };
+        run_handler(handler_fn);
+    }
+    ShutdownAPI(options);
+    return 0;
+}
+
+/*uint64_t download_file(redisContext* context,
+                       std::string const &key)
+{
+    int retries = 0;
+    const int MAX_RETRIES = 50000;
+    while (retries < MAX_RETRIES) {
+        std::string comm = "GET " + key;
+        redisReply* reply = (redisReply*) redisCommand(context, comm.c_str());
+        if (reply->type == REDIS_REPLY_NIL || reply->type == REDIS_REPLY_ERROR) {
+            retries += 1;
+            freeReplyObject(reply);
+        } else {
+            uint64_t finishedTime = timeSinceEpochMillisec();
+            // Perform NOP on result to prevent optimizations
+            for (int i = 0; i < reply->elements; i++) {
+                std::string res = reply->element[i]->str;
+            }
+            freeReplyObject(reply);
+            return finishedTime;
+        }
+    }
+    return 0;
+
+}
+
+uint64_t upload_random_file(redisContext* context,
+                        std::string const &key,
+                        int size)
+{
+    char* pBuf = new char[size];
+
+    uint64_t bef_upload = timeSinceEpochMillisec();
+    std::string comm = "SET " + key + " %b";
+    redisReply* reply = (redisReply*) redisCommand(context, comm.c_str(), pBuf, size);
+    freeReplyObject(reply);
+    delete[] pBuf;
+    return bef_upload;
+}*/
