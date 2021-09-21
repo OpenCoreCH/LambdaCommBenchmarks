76909c3a16f9a79d7007c71a5404c326052c83c9
Roman Böhringer <r.boehringer@opencore.ch>
2021-09-21 20:13:29 +0200

    Ran NAT scaling benchmarks with up to 256 lambdas

--- nat/one_to_many/scaling_data/invoke.sh
+++ nat/one_to_many/scaling_data/invoke.sh
@@ -3,12 +3,11 @@ mkdir -p out/
 
 SERVER_IP=18.184.154.126
 filesizes=(1 1000 10000 100000 1000000 10000000 100000000 500000000)
-filesizes=(500000000)
 consumer=8
 
 for filesize in "${filesizes[@]}"
 do
-    for run in {5..5}
+    for run in {1..10}
     do
         timestamp=$(date +%s)
         aws lambda invoke --function-name natbenchmark --cli-read-timeout 120 --cli-connect-timeout 120 --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"$timestamp"'", "role": "producer", "fileSize": '"$filesize"', "numConsumers":'"$consumer"' }' "out/producer_${filesize}_${run}.json" &
---
+++ nat/one_to_many/scaling_lambdas/invoke.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+mkdir -p out/
+SERVER_IP=18.184.154.126
+
+filesizes=(1 1000 10000 100000 1000000 10000000 500000000)
+filesize=10000000
+#consumers=(1 2 4 8 16 32 64 128 256)
+consumers=(128)
+
+for consumer in "${consumers[@]}"
+    do
+    mkdir -p "out/$consumer"
+    for run in {1..10}
+        do
+            timestamp=$(date +%s)
+            aws lambda invoke --function-name natbenchmark --cli-read-timeout 180 --cli-connect-timeout 180 --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"$timestamp"'", "role": "producer", "fileSize": '"$filesize"', "numConsumers":'"$consumer"' }' "out/$consumer/producer_${filesize}_${run}.json" &
+            for consumernum in $(seq 1 $consumer);
+                do
+                aws lambda invoke --function-name natbenchmark --cli-read-timeout 180 --cli-connect-timeout 180 --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"${timestamp}_${consumernum}"'", "role": "consumer", "fileSize": '"$filesize"' }' "out/$consumer/consumer_${filesize}_${run}_${consumernum}.json" &
+                done
+            if [ "$consumer" -gt "100" ]; then
+                sleep 190
+            else
+                sleep 20
+            fi
+        done
+    done
