853ea754111b8f32dcdbd3a72f8140c534a2fb56
Roman Böhringer <r.boehringer@opencore.ch>
2021-10-12 21:53:47 +0200

    Ran comparison with MPI on VMs

---
+++ mpi/mpi_benchmark.c
@@ -0,0 +1,105 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <string.h>
+#include <stdlib.h>
+
+unsigned long get_time_in_microseconds() {
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return tv.tv_usec;
+    return 1000000 * tv.tv_sec + tv.tv_usec;
+}
+
+void bcast_benchmark(int bcast_value) {
+    int bcast_result;
+    MPI_Bcast(&bcast_result, 1, MPI_INT, 0, MPI_COMM_WORLD);
+}
+
+void gather_benchmark(void* send_buf, int send_count, void* recv_buf, int recv_count) {
+    MPI_Gather(send_buf, send_count, MPI_INT, recv_buf, recv_count, MPI_INT, 0, MPI_COMM_WORLD);
+}
+
+void scatter_benchmark(void* send_buf, int send_count, void* recv_buf, int recv_count) {
+    MPI_Scatter(send_buf, send_count, MPI_INT, recv_buf, recv_count, MPI_INT, 0, MPI_COMM_WORLD);
+}
+
+void reduce_benchmark(int red_value) {
+    int reduction_result;
+    MPI_Reduce(&red_value, &reduction_result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+}
+
+void allreduce_benchmark(int red_value) {
+    int reduction_result;
+    MPI_Allreduce(&red_value, &reduction_result, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+}
+
+void scan_benchmark(int red_value) {
+    int reduction_result;
+    MPI_Scan(&red_value, &reduction_result, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+}
+
+int main(int argc, char** argv) {
+    MPI_Init(NULL, NULL);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    for (int i = 0; i < 1001; i++) {
+        unsigned long bef, after;
+        if (strcmp(argv[1], "bcast") == 0) {
+            bef = get_time_in_microseconds();
+            bcast_benchmark(world_rank + 1);
+            after = get_time_in_microseconds();
+        } else if (strcmp(argv[1], "gather") == 0) {
+            long recv_size = 5000;
+            long individual_size = recv_size / world_size;
+            int* buffer = malloc(individual_size * sizeof(int));
+            void * recv_buffer;
+            if (world_rank == 0)
+                recv_buffer = malloc(recv_size * sizeof(int));
+            bef = get_time_in_microseconds();
+            gather_benchmark(buffer, individual_size, recv_buffer, individual_size);
+            after = get_time_in_microseconds();
+            free(buffer);
+            if (world_rank == 0)
+                free(recv_buffer);
+        } else if (strcmp(argv[1], "scatter") == 0) {
+            long send_size = 5000;
+            long individual_size = send_size / world_size;
+            int* buffer;
+            if (world_rank == 0)
+                buffer = malloc(send_size * sizeof(int));
+            int* recv_buffer = malloc(individual_size * sizeof(int));
+            bef = get_time_in_microseconds();
+            scatter_benchmark(buffer, individual_size, recv_buffer, individual_size);
+            after = get_time_in_microseconds();
+            free(recv_buffer);
+            if (world_rank == 0)
+                free(buffer);
+        } else if (strcmp(argv[1], "reduce") == 0) {
+            bef = get_time_in_microseconds();
+            reduce_benchmark(world_rank + 1);
+            after = get_time_in_microseconds();
+        } else if (strcmp(argv[1], "allreduce") == 0) {
+            bef = get_time_in_microseconds();
+            allreduce_benchmark(world_rank + 1);
+            after = get_time_in_microseconds();
+        } else if (strcmp(argv[1], "scan") == 0) {
+            bef = get_time_in_microseconds();
+            scan_benchmark(world_rank + 1);
+            after = get_time_in_microseconds();
+        }
+
+        if (i > 0)
+            printf("%lu\n", after - bef);
+    }
+    
+
+    MPI_Finalize();
+}
+
+
---
+++ smi/smi_benchmark.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <sys/time.h>
+#include <Communicator.h>
+
+unsigned long get_time_in_microseconds() {
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return tv.tv_usec;
+    return 1000000 * tv.tv_sec + tv.tv_usec;
+}
+
+void bcast_benchmark(SMI::Comm::Data<int>& data, SMI::Communicator& comm) {
+    comm.bcast(data, 0);
+}
+
+void gather_benchmark(SMI::Comm::Data<std::vector<int>>& sendbuf, SMI::Comm::Data<std::vector<int>>& recvbuf, SMI::Communicator& comm) {
+    comm.gather(sendbuf, recvbuf, 0);
+}
+
+void scatter_benchmark(SMI::Comm::Data<std::vector<int>>& sendbuf, SMI::Comm::Data<std::vector<int>>& recvbuf, SMI::Communicator& comm) {
+    comm.scatter(sendbuf, recvbuf, 0);
+}
+
+void reduce_benchmark(SMI::Comm::Data<int>& data, SMI::Communicator& comm) {
+    SMI::Comm::Data<int> res;
+    SMI::Utils::Function<int> f([] (auto a, auto b) {return a + b;}, true, true);
+    comm.reduce(data, res, 0, f);
+}
+
+void allreduce_benchmark(SMI::Comm::Data<int>& data, SMI::Communicator& comm) {
+    SMI::Comm::Data<int> res;
+    SMI::Utils::Function<int> f([] (auto a, auto b) {return a + b;}, true, true);
+    comm.allreduce(data, res, f);
+}
+
+void scan_benchmark(SMI::Comm::Data<int>& data, SMI::Communicator& comm) {
+    SMI::Comm::Data<int> res;
+    SMI::Utils::Function<int> f([] (auto a, auto b) {return a + b;}, true, true);
+    comm.scan(data, res, f);
+}
+
+int main(int argc, char** argv) {
+    std::string benchmark(argv[1]);
+    int num_peers = std::stoi(argv[2]);
+    int peer_id = std::stoi(argv[3]);
+
+    SMI::Communicator comm(peer_id, num_peers, "smi.json", "SMITest", 512);
+    comm.barrier();
+    
+
+    for (int i = 0; i < 1001; i++) {
+        unsigned long bef, after;
+        if (benchmark == "bcast") {
+            SMI::Comm::Data<int> data = peer_id + 1;
+            bef = get_time_in_microseconds();
+            bcast_benchmark(data, comm);
+            after = get_time_in_microseconds();
+        } else if (benchmark == "gather") {
+            long recv_size = 5000;
+            long individual_size = recv_size / num_peers;
+            SMI::Comm::Data<std::vector<int>> recv(recv_size);
+            SMI::Comm::Data<std::vector<int>> send(individual_size);
+            bef = get_time_in_microseconds();
+            gather_benchmark(send, recv, comm);
+            after = get_time_in_microseconds();
+        } else if (benchmark == "scatter") {
+            long send_size = 5000;
+            long individual_size = send_size / num_peers;
+            SMI::Comm::Data<std::vector<int>> recv(individual_size);
+            SMI::Comm::Data<std::vector<int>> send(send_size);
+            bef = get_time_in_microseconds();
+            scatter_benchmark(send, recv, comm);
+            after = get_time_in_microseconds();
+        } else if (benchmark == "reduce") {
+            SMI::Comm::Data<int> data = peer_id + 1;
+            bef = get_time_in_microseconds();
+            reduce_benchmark(data, comm);
+            after = get_time_in_microseconds();
+        } else if (benchmark == "allreduce") {
+            SMI::Comm::Data<int> data = peer_id + 1;
+            bef = get_time_in_microseconds();
+            allreduce_benchmark(data, comm);
+            after = get_time_in_microseconds();
+        } else if (benchmark == "scan") {
+            SMI::Comm::Data<int> data = peer_id + 1;
+            bef = get_time_in_microseconds();
+            scan_benchmark(data, comm);
+            after = get_time_in_microseconds();
+        }
+        if (i > 0)
+            std::cout << after - bef << std::endl;
+        
+    }
+}
+
+
