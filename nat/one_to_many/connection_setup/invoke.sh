aaaf616d0d275d2d21e731cbe0e031ee1eafc403
Roman Böhringer <r.boehringer@opencore.ch>
2021-10-22 22:09:06 +0200

    Whole analysis of experiment

--- nat/one_to_many/connection_setup/invoke.sh
+++ nat/one_to_many/connection_setup/invoke.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 mkdir -p out/
-SERVER_IP=3.120.108.197
+SERVER_IP=52.59.239.255
 
-consumers=(1 2 4 8 16 32 64 128)
+consumers=(1)
 
 for consumer in "${consumers[@]}"
     do
     mkdir -p "out/$consumer"
-    for run in {1..10}
+    for run in {11..11}
         do
             timestamp=$(date +%s)
             aws lambda invoke --function-name natbenchmarksetup --cli-read-timeout 180 --cli-connect-timeout 180 --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"$timestamp"'", "role": "producer", "numConsumers":'"$consumer"' }' "out/$consumer/producer_${run}.json" &
--- nat/one_to_many/scaling_lambdas/invoke_overhead.sh
+++ nat/one_to_many/scaling_lambdas/invoke_overhead.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 mkdir -p out_overhead/
-SERVER_IP=52.59.247.14
+SERVER_IP=52.59.239.255
 
 filesizes=(1 1000 10000 100000 1000000 10000000 500000000)
-filesize=1000
+filesize=1
 consumers=(64)
 
 for consumer in "${consumers[@]}"
     do
     mkdir -p "out_overhead/$consumer"
-    for run in {2..2}
+    for run in {11..11}
         do
             timestamp=$(date +%s)
             aws lambda invoke --function-name natbenchmark --cli-read-timeout 180 --cli-connect-timeout 180 --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"$timestamp"'", "role": "producer", "fileSize": '"$filesize"', "numConsumers":'"$consumer"' }' "out_overhead/$consumer/producer_${filesize}_${run}.json" &
