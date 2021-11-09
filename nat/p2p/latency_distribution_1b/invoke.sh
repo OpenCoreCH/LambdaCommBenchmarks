cea6f259541ae02dca903a0960378628bb323464
Roman Böhringer <r.boehringer@opencore.ch>
2021-11-09 12:17:21 +0100

    NAT benchmark with different RAM

--- nat/p2p/latency_distribution_1b/invoke.sh
+++ nat/p2p/latency_distribution_1b/invoke.sh
@@ -6,7 +6,7 @@ SERVER_IP=35.156.180.65
 filesizes=(1 1000 10000 100000 1000000 10000000 100000000)
 filesize=1
 
-for run in {1..100}
+for run in {76..76}
     do
         timestamp=$(date +%s)
         aws lambda invoke --function-name natbenchmark --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"${timestamp}_1"'", "role": "consumer", "fileSize": '"$filesize"' }' "out/consumer_${filesize}_${run}.json" &
--- nat/p2p/scaling/invoke.sh
+++ nat/p2p/scaling/invoke.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 mkdir -p out/
-SERVER_IP=52.59.227.123
+SERVER_IP=35.156.180.65
 
-filesizes=(500000000)
+filesizes=(1 1000 10000 100000 1000000 10000000 100000000)
 
 for filesize in "${filesizes[@]}"
 do
-    for run in {3..3}
+    for run in {1..10}
     do
         timestamp=$(date +%s)
-        aws lambda invoke --function-name natbenchmark --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"$timestamp"'", "role": "producer", "fileSize": '"$filesize"' }' "out/producer_${filesize}_${run}.json" &
-        aws lambda invoke --function-name natbenchmark --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"${timestamp}_1"'", "role": "consumer", "fileSize": '"$filesize"' }' "out/consumer_${filesize}_${run}.json" &
-        if [ "$filesize" -gt "100000000" ]; then
-            sleep 25
-        else
-            sleep 10
-        fi
+        aws lambda invoke --function-name natbenchmark --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"$timestamp"'", "role": "producer", "fileSize": '"$filesize"' }' "out_512ram/producer_${filesize}_${run}.json" &
+        aws lambda invoke --function-name natbenchmark --cli-binary-format raw-in-base64-out --payload '{"ip":"'"$SERVER_IP"'", "key":"'"${timestamp}_1"'", "role": "consumer", "fileSize": '"$filesize"' }' "out_512ram/consumer_${filesize}_${run}.json" &
+        sleep 10
     done
 done
